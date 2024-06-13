6d65e5a08e6ae8c4b582defb8ab5676e56ce748e
tv0924@icloud.com <j93es@naver.com>
2024-06-13 21:17:53 +0900

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#dldld
 
-mkdir aws_ddns_$(date +%Y%m%d%H%M%S).bak &&
-mv aws_ddns_main.py src update.sh aws_ddns_$(date +%Y%m%d%H%M%S).bak &&
+mkdir aws_ddns_$(date +%Y%m%d%H%M%S).bak || true &&
+mv aws_ddns_main.py src aws_ddns_$(date +%Y%m%d%H%M%S).bak || true &&
 git clone https://github.com/J93es/aws_ddns.git tmp_aws_ddns &&
-mv tmp_aws_ddns/MY_PATH/aws_ddns_main.py tmp_aws_ddns/MY_PATH/src tmp_aws_ddns/MY_PATH/update.sh . &&
+mv tmp_aws_ddns/MY_PATH/aws_ddns_main.py tmp_aws_ddns/MY_PATH/src . &&
 chmod 755 aws_ddns_main.py start.sh &&
 rm -rf tmp_aws_ddns &&
 
