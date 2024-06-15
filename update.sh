feeca0a0b8e067b0f076d2db00337e9fdc0050e9
tv0924@icloud.com <j93es@naver.com>
2024-06-15 18:50:24 +0900

    update: start.sh update.sh aws_ddns.service

--- update.sh
+++ update.sh
@@ -5,16 +5,16 @@ BACKUP_NAME="aws_ddns_$(date +%Y%m%d%H%M%S).bak/"
 AWS_DDNS_GIT_URL="https://github.com/J93es/aws_ddns.git"
 TMP_DIR="__tmp_aws_ddns/"
 
-sudo mkdir -p           ${BACKUP_DIR}/${BACKUP_NAME} &&
-sudo mv -r src          ${BACKUP_DIR}/${BACKUP_NAME} &&
-sudo cp -p config.json  ${BACKUP_DIR}/${BACKUP_NAME} &&
-sudo cp -p start.sh     ${BACKUP_DIR}/${BACKUP_NAME} &&
+sudo mkdir -p                               ${BACKUP_DIR}/${BACKUP_NAME}            &&
+sudo mv src                                 ${BACKUP_DIR}/${BACKUP_NAME}    || true &&
+sudo cp -p          config.json             ${BACKUP_DIR}/${BACKUP_NAME}    || true &&
+sudo cp -p          start.sh                ${BACKUP_DIR}/${BACKUP_NAME}    || true &&
 
-sudo rm -r                          ${TMP_DIR} &&
-sudo git clone ${AWS_DDNS_GIT_URL}  ${TMP_DIR} &&
-sudo mv -r                          ${TMP_DIR}/src . &&
-sudo rm -r                          ${TMP_DIR} &&
+sudo rm -rf         ${TMP_DIR} &&
+sudo git clone      ${AWS_DDNS_GIT_URL}     ${TMP_DIR}                              &&
+sudo mv             ${TMP_DIR}/src          ./src                                   &&
+sudo rm -rf         ${TMP_DIR}                                                      &&
 
-sudo systemctl daemon-reload &&
-sudo systemctl restart aws_ddns.service &&
-sudo systemctl restart aws_ddns.timer
+sudo systemctl      daemon-reload                                                   &&
+sudo systemctl      restart                 aws_ddns.service                        &&
+sudo systemctl      restart                 aws_ddns.timer
